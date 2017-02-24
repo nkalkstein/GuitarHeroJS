d2d613f192f774ec152543c100b1ef4ca2c19d0c
clement.coulon@orange.fr <clement.coulon@orange.fr>
2017-02-24 19:28:34 +0100

    backgoud + dossier img

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 var GAME_START = false;
 var GAME_OVER = false;
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'Guitar Hero');
+var game = new Phaser.Game(600, 800, Phaser.AUTO, 'GuitarHero');
 
 
+var GuitarHero = {
+	preload: function (){ //Chargement des ressources 
+		game.load.image('background', 'img/background.png');
+	},
+	
+	create: function () { //Affichage des ressources 
+		game.add.sprite(0, 0, 'background');
+	},
+	
+	update : function (){
+		
+	}
+};
+
+game.state.add('GuitarHero', GuitarHero);
+game.state.start('GuitarHero'); 
