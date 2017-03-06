b1797ae83a69ccfb591f33afdcf511c897fd29c5
clement.coulon@orange.fr <clement.coulon@orange.fr>
2017-03-06 18:33:53 +0100

    test function position + test du code sur score: augmente à chaque appartion de boule bleu

--- main.js
+++ main.js
@@ -6,13 +6,17 @@ function preload() {
 	game.load.image('boule_verte' , 'img/boule_vert3.png');
 	game.load.image('boule_rouge' , 'img/boule_rouge3.png');
 	game.load.image('boule_jaune' , 'img/boule_jaune3.png');
-	
+	game.load.image('sprite_test' , 'img/sprite_test.png');
 }
 
 var boule_bleu
 var boule_rouge	
 var boule_verte
 var boule_jaune
+
+var sprite_test
+var score
+var spacekey
 
 var text
 
@@ -21,6 +25,10 @@ function create() {
 	game.physics.arcade.gravity.y = 100;   
 
 	game.add.sprite(0,0, 'bg');
+	
+	game.add.sprite(115,644, 'sprite_test');
+	
+
 	
 	//boule_bleu = game.add.sprite(82, 10, 'boule_bleu');
 	//on upscale car trop petit
@@ -56,8 +64,9 @@ function create() {
 	//boule_jaune.outOfBoundsKill = true;
 	
 	//game.physics.enable( [boule_bleu, boule_verte, boule_rouge, boule_jaune], Phaser.Physics.ARCADE);
-
-	text = game.add.text(game.world.centerX, game.world.centerY, 'test :');
+	
+	score = 0;
+	text = game.add.text(game.world.centerX, game.world.centerY, 'score :');
 	
 
 	//game.time.events.loop(100, crea_nombre_rand, this);
@@ -72,7 +81,7 @@ function create() {
 	game.time.events.loop(game.rnd.integerInRange(500, 2000), crea_sprite_jaune, this);
 	
 	
-	
+	this.spacekey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 
 	
 }
@@ -82,6 +91,12 @@ function update() {
 	
 }
 
+function position_sprite_validation() {
+	if (game.physics.arcade.distanceBetween(boule_bleu, sprite_test) > 30  &&  this.spacekey.isDown){
+		score += 10;
+		text.setText('score : 0' + score);
+	};
+}
 
 
 //var random_number_bleu
@@ -92,15 +107,18 @@ function update() {
 //}
 
 function crea_sprite_bleu() {
-	var boule_bleu_rand = game.add.sprite(82, -200, 'boule_bleu');
+	var boule_bleu_rand = game.add.sprite(82, -75, 'boule_bleu');
 	boule_bleu_rand.scale.setTo(1.5,1.5);
 	game.physics.enable( [boule_bleu_rand], Phaser.Physics.ARCADE);
 	boule_bleu_rand.body.gravity.y=40;
 	
+	score += 10;
+	text.setText('score :' + score);
+	
 }
 
 function crea_sprite_verte() {
-	var boule_verte_rand = game.add.sprite(205, -200, 'boule_verte');
+	var boule_verte_rand = game.add.sprite(205, -75, 'boule_verte');
 	boule_verte_rand.scale.setTo(1.5,1.5);
 	game.physics.enable( [boule_verte_rand], Phaser.Physics.ARCADE);
 	boule_verte_rand.body.gravity.y=40;
@@ -108,7 +126,7 @@ function crea_sprite_verte() {
 }
 
 function crea_sprite_rouge() {
-	var boule_rouge_rand = game.add.sprite(328, -200, 'boule_rouge');
+	var boule_rouge_rand = game.add.sprite(328, -75, 'boule_rouge');
 	boule_rouge_rand.scale.setTo(1.5,1.5);
 	game.physics.enable( [boule_rouge_rand], Phaser.Physics.ARCADE);
 	boule_rouge_rand.body.gravity.y=40;
@@ -116,7 +134,7 @@ function crea_sprite_rouge() {
 }
 
 function crea_sprite_jaune() {
-	var boule_jaune_rand = game.add.sprite(450, -200, 'boule_jaune');
+	var boule_jaune_rand = game.add.sprite(450, -75, 'boule_jaune');
 	boule_jaune_rand.scale.setTo(1.5,1.5);
 	game.physics.enable( [boule_jaune_rand], Phaser.Physics.ARCADE);
 	boule_jaune_rand.body.gravity.y=40;
