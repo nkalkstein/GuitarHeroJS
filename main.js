8e154c4164bdcca0af929f6d5268f84366d13abf
Clément <clement.coulon@orange.fr>
2017-04-28 11:16:23 +0200

    test of new distance check

--- main.js
+++ main.js
@@ -9,14 +9,12 @@ function preload() {
 	game.load.image('point_test' , 'img/sprite_test.png');
 }
 
-var boule_bleu
-var boule_rouge	
-var boule_verte
-var boule_jaune
 
 var point_test
-var score
-var bouleBleuKey
+var blue_boule_group
+
+
+
 
 var text
 
@@ -29,62 +27,34 @@ function create() {
 	game.add.sprite(115,644, 'point_test'); //--> ajout d'un nouveau sprite pour mesurer la distance entre ce sprite et la boule bleu
 	
 
-	
-	//boule_bleu = game.add.sprite(82, 10, 'boule_bleu');
-	//on upscale car trop petit
-	//boule_bleu.scale.setTo(1.5,1.5);
-	
-	
-	//boule_verte = game.add.sprite(205, 10, 'boule_verte');
-	//boule_verte.scale.setTo(1.5,1.5);
-	
-	
-	//boule_rouge = game.add.sprite(328, 10, 'boule_rouge');
-	//boule_rouge.scale.setTo(1.5,1.5);
-
-	
-	//boule_jaune = game.add.sprite(450, 10, 'boule_jaune');
-	//boule_jaune.scale.setTo(1.5,1.5);	
-	
-	
-	//boule_bleu.checkWorldBounds = true; //--> check si la boule est dans les bornes du jeu 
-	//boule_bleu.outOfBoundsKill = true; //--> si en dehors des bornes du jeu alors on kill la boule
-	
-	//boule_verte.checkWorldBounds = true;
-	//boule_verte.outOfBoundsKill = true;
-	
-	//boule_rouge.checkWorldBounds = true;
-	//boule_rouge.outOfBoundsKill = true;
-	
-	//boule_jaune.checkWorldBounds = true;
-	//boule_jaune.outOfBoundsKill = true;
-	
-	//game.physics.enable( [boule_bleu, boule_verte, boule_rouge, boule_jaune], Phaser.Physics.ARCADE);
-	
-	//game.time.events.loop(100, crea_nombre_rand, this);
-	
-
-	//game.time.events.loop(game.rnd.integerInRange(400, 3000), crea_sprite_bleu, this);
-	
-	//game.time.events.loop(game.rnd.integerInRange(600, 2000), crea_sprite_verte, this);
-	
-	//game.time.events.loop(game.rnd.integerInRange(600, 2000), crea_sprite_rouge, this);
-	
-	//game.time.events.loop(game.rnd.integerInRange(800, 2000), crea_sprite_jaune, this);
-	
-	//game.physics.enable( [sprite_test], Phaser.Physics.ARCADE);
-	
 	text = game.add.text(game.world.centerX, game.world.centerY, 'test :');
 	
 	game.time.events.loop(1000, crea_nombre_rand, this);
+	var score = 0;
+	
+	blue_boule_group = game.add.group();
+	
 	
 }
 
 
 function update() {
-	
+	if (game.input.keyboard.isDown(Phaser.Keyboard.A))
+	{
+
+		if (blue_boule_group.getChildAt(-1).y>500&&<blue_boule_group.getChildAt(-1).y<600)
+		{
+			score+=1;
+		}
+		if (blue_boule_group.getChildAt(-1).y>=600&&<blue_boule_group.getChildAt(-1).y<700)
+		{
+			score+=5; 
+		}
+	}
 }
+
 var random_number
+
 function crea_nombre_rand() {
 	random_number = game.rnd.integerInRange(1, 4) ;
 	text.setText('test :' + random_number);
@@ -108,51 +78,17 @@ function crea_nombre_rand() {
 
 
 
-//var random_number_bleu
-
-//function crea_nombre_rand() {
-//	random_number_bleu = game.rnd.integerInRange(50, 200)/100 ; //generation de 
-//	text.setText('test :' + random_number_bleu);
-//}
-
 function crea_sprite_bleu() {
 	var boule_bleu_rand = game.add.sprite(82, -75, 'boule_bleu');
 	boule_bleu_rand.scale.setTo(1.5,1.5);
 	game.physics.enable( [boule_bleu_rand], Phaser.Physics.ARCADE);
 	boule_bleu_rand.body.gravity.y=40;
+	blue_boule_group.add(boule_bleu_rand);
 	
 }
 
-function crea_sprite_verte() {
-	var boule_verte_rand = game.add.sprite(205, -75, 'boule_verte');
-	boule_verte_rand.scale.setTo(1.5,1.5);
-	game.physics.enable( [boule_verte_rand], Phaser.Physics.ARCADE);
-	boule_verte_rand.body.gravity.y=40;
-	
-}
-
-function crea_sprite_rouge() {
-	var boule_rouge_rand = game.add.sprite(328, -75, 'boule_rouge');
-	boule_rouge_rand.scale.setTo(1.5,1.5);
-	game.physics.enable( [boule_rouge_rand], Phaser.Physics.ARCADE);
-	boule_rouge_rand.body.gravity.y=40;
-	
-}
-
-function crea_sprite_jaune() {
-	var boule_jaune_rand = game.add.sprite(450, -75, 'boule_jaune');
-	boule_jaune_rand.scale.setTo(1.5,1.5);
-	game.physics.enable( [boule_jaune_rand], Phaser.Physics.ARCADE);
-	boule_jaune_rand.body.gravity.y=40;
-	
-}
 
 
 function render() {
-
-    // debug info pour les boules
-   // game.debug.spriteInfo(boule_jaune, 32, 32);
-	
-
 
 }
