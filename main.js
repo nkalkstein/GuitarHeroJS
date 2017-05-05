a00ebda3d9fe270b66ef8e908203c4602b399b3b
Clément <clement.coulon@orange.fr>
2017-05-05 11:36:12 +0200

    english trad

--- main.js
+++ main.js
@@ -2,18 +2,18 @@ var game = new Phaser.Game(600, 800, Phaser.AUTO, 'GuitarHeroJSGame', { preload:
 
 function preload() {
 	game.load.image('bg' , 'img/background.png');
-	game.load.image('boule_bleu' , 'img/boule_bleu3.png');
-	game.load.image('boule_verte' , 'img/boule_vert3.png');
-	game.load.image('boule_rouge' , 'img/boule_rouge3.png');
-	game.load.image('boule_jaune' , 'img/boule_jaune3.png');
-	game.load.image('point_test' , 'img/sprite_test.png');
+	game.load.image('blue_ball' , 'img/blue_ball.png');
+	game.load.image('green_ball' , 'img/green_ball.png');
+	game.load.image('red_ball' , 'img/red_ball.png');
+	game.load.image('yellow_ball' , 'img/yellow_ball.png');
+	
 }
 
 
 
 var point_test
 var score
-var bouleBleuKey
+var bouleblueKey
 
 var text
 var Blue_button
@@ -24,110 +24,110 @@ function create() {
 
 	game.add.sprite(0,0, 'bg');
 	
-	game.add.sprite(115,644, 'point_test'); //--> ajout d'un nouveau sprite pour mesurer la distance entre ce sprite et la boule bleu
+	
 	
 	text = game.add.text(game.world.centerX, game.world.centerY, 'test :');
 	
-	master_group = game.add.group();
-	
-	blue_boule_group = game.add.group();
-	green_boule_group = game.add.group();
-	red_boule_group = game.add.group();
-	yellow_boule_group = game.add.group();
+	blue_ball_group = game.add.group();
+	green_ball_group = game.add.group();
+	red_ball_group = game.add.group();
+	yellow_ball_group = game.add.group();
 	
 	
+	crea_sprite_blue();
+	crea_sprite_green();
+	crea_sprite_red();
+	crea_sprite_yellow();
 	
-	crea_sprite_bleu();
-	
-	game.time.events.loop(1000, crea_nombre_rand, this);
+	game.time.events.loop(1000, crea_number_rand, this);
 	
 	score = 1;
 	scoreText = game.add.text(game.world.centerX, game.world.centerY-50, 'score :'+	score);
 	
-	Blue_button= game.input.keyboard.addKey(Phaser.Keyboard.A);
-	Blue_button.onDown.add(addScoreBlue, this);
+	blue_button= game.input.keyboard.addKey(Phaser.Keyboard.A);
+	blue_button.onDown.add(addScoreBlue, this);
 	
 	
 }
 
 function addScoreBlue () {
-	if (blue_boule_group.getChildAt(0).y>500&&blue_boule_group.getChildAt(0).y<600)
+	if (blue_ball_group.getChildAt(0).y>500&&blue_ball_group.getChildAt(0).y<600)
 	{
 			score+=1;
-			blue_boule_group.getChildAt(0).destroy();
+			blue_ball_group.getChildAt(0).destroy();
 			scoreText.setText('score :' + score);
 	}
-	if (blue_boule_group.getChildAt(0).y>=600&&blue_boule_group.getChildAt(0).y<700)
+	if (blue_ball_group.getChildAt(0).y>=600&&blue_ball_group.getChildAt(0).y<700)
 	{
 		score+=5; 
-		blue_boule_group.getChildAt(0).destroy();	
+		blue_ball_group.getChildAt(0).destroy();	
 		scoreText.setText('score :' + score);
 	}	
 }
 
 function update() {
-	if (blue_boule_group.getChildAt(0).y>800)
+	if (blue_ball_group.getChildAt(0).y>800)
 	{
-		blue_boule_group.getChildAt(0).destroy();
+		blue_ball_group.getChildAt(0).destroy();
 		score-=1; 
 		scoreText.setText('score :' + score);
 	
 	}
 }
 var random_number
-function crea_nombre_rand() {
+function crea_number_rand() {
 	random_number = game.rnd.integerInRange(1, 2) ;
 	text.setText('test :' + random_number);
 	if (random_number == 1)
 	{
-		crea_sprite_bleu();
+		crea_sprite_blue();
 	}
 	if (random_number == 2)
 	{
-		crea_sprite_verte();
+		crea_sprite_green();
 	}
 	if (random_number == 3)
 	{
-		crea_sprite_rouge();
+		crea_sprite_red();
 	}
 	if (random_number == 4)
 	{
-		crea_sprite_jaune();
+		crea_sprite_yellow();
 	}
 }
 
-function crea_sprite_bleu() {
-	var boule_bleu_rand = game.add.sprite(82, -75, 'boule_bleu');
-	boule_bleu_rand.scale.setTo(1.5,1.5);
-	game.physics.enable( [boule_bleu_rand], Phaser.Physics.ARCADE);
-	boule_bleu_rand.body.gravity.y=40;
-	blue_boule_group.add(boule_bleu_rand);
-	master_group.add(blue_boule_group);
+function crea_sprite_blue() {
+	var blue_ball_rand = game.add.sprite(82, -75, 'blue_ball');
+	blue_ball_rand.scale.setTo(1.5,1.5);
+	game.physics.enable( [blue_ball_rand], Phaser.Physics.ARCADE);
+	blue_ball_rand.body.gravity.y=40;
+	blue_ball_group.add(blue_ball_rand);
+	
 	
 }
 
-function crea_sprite_verte() {
-	var boule_verte_rand = game.add.sprite(205, -75, 'boule_verte');
-	boule_verte_rand.scale.setTo(1.5,1.5);
-	game.physics.enable( [boule_verte_rand], Phaser.Physics.ARCADE);
-	boule_verte_rand.body.gravity.y=40;
-	green_boule_group.add(boule_verte_rand);
-	master_group.add(green_boule_group);
-}
-
-function crea_sprite_rouge() {
-	var boule_rouge_rand = game.add.sprite(328, -75, 'boule_rouge');
-	boule_rouge_rand.scale.setTo(1.5,1.5);
-	game.physics.enable( [boule_rouge_rand], Phaser.Physics.ARCADE);
-	boule_rouge_rand.body.gravity.y=40;
+function crea_sprite_green() {
+	var green_ball_rand = game.add.sprite(205, -75, 'green_ball');
+	green_ball_rand.scale.setTo(1.5,1.5);
+	game.physics.enable( [green_ball_rand], Phaser.Physics.ARCADE);
+	green_ball_rand.body.gravity.y=40;
+	green_ball_group.add(green_ball_rand);
 	
 }
 
-function crea_sprite_jaune() {
-	var boule_jaune_rand = game.add.sprite(450, -75, 'boule_jaune');
-	boule_jaune_rand.scale.setTo(1.5,1.5);
-	game.physics.enable( [boule_jaune_rand], Phaser.Physics.ARCADE);
-	boule_jaune_rand.body.gravity.y=40;
+function crea_sprite_red() {
+	var red_ball_rand = game.add.sprite(328, -75, 'red_ball');
+	red_ball_rand.scale.setTo(1.5,1.5);
+	game.physics.enable( [red_ball_rand], Phaser.Physics.ARCADE);
+	red_ball_rand.body.gravity.y=40;
+	
+}
+
+function crea_sprite_yellow() {
+	var yellow_ball_rand = game.add.sprite(450, -75, 'yellow_ball');
+	yellow_ball_rand.scale.setTo(1.5,1.5);
+	game.physics.enable( [yellow_ball_rand], Phaser.Physics.ARCADE);
+	yellow_ball_rand.body.gravity.y=40;
 	
 }
 
@@ -135,7 +135,7 @@ function crea_sprite_jaune() {
 function render() {
 
     // debug info pour les boules
-   game.debug.spriteInfo(blue_boule_group.getChildAt(0), 32, 32);
+   game.debug.spriteInfo(blue_ball_group.getChildAt(0), 32, 32);
 	
 
 
